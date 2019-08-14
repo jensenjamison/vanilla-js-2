b0748bb45a9fae57e1ab0de589052d466ba86117
jensenjamison <jensenjamison@yahoo.com>
2019-08-14 11:24:37 -0500

    grocery list still in progress

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("button").onclick = function(e) {
+
+}
+
+// function addToList() {
+//     let input = document.getElementById("item-input");
+//     let inputText = input.value;
+//     let element = document.createElement("li");
+//     element.innerText = inputText;
+//     document.getElementById("todo-list").append(element);
+//     // `<li>${inputText}</li>`
+// }
